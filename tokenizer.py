ef3ead74d613635e8784b636c653f49c5c8f9fec
Thien Tran <gau.nernst@yahoo.com.sg>
2023-08-05 23:45:08 +0800

    add tokenizer

---
+++ tokenizer.py
@@ -0,0 +1,47 @@
+import argparse
+
+import numpy as np
+import sentencepiece as spm
+
+
+if __name__ == "__main__":
+    # https://github.com/google/sentencepiece/blob/master/doc/options.md
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action")
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--save_path")
+    parser.add_argument("--model_name", required=True)
+    parser.add_argument("--model_type", default="unigram")
+    parser.add_argument("--vocab_size", type=int, default=8000)
+    parser.add_argument("--input_sentence_size", type=int, default=0)
+    parser.add_argument("--max_sentence_length", type=int, default=4192)
+    args = parser.parse_args()
+
+    if args.action == "train":
+        spm.SentencePieceTrainer.train(
+            input=args.input,
+            model_prefix=args.model_name,
+            model_type=args.model_type,
+            vocab_size=args.vocab_size,
+            character_coverage=1.0,
+            input_sentence_size=args.input_sentence_size,
+            max_sentence_length=args.max_sentence_length,
+            shuffle_input_sentence=(args.input_sentence_size > 0),
+        )
+
+    elif args.action == "tokenize":
+        assert args.save_path is not None
+        tokenizer = spm.SentencePieceProcessor(f"{args.model_name}.model")
+        vocab_size = tokenizer.vocab_size()
+        assert vocab_size < 2**16
+
+        stories = open(args.input).read().split("<|endoftext|>")
+        tokens = tokenizer.Encode(stories, add_bos=True)
+        size = sum(len(x) for x in tokens)
+        mmap = np.memmap(args.save_path, np.uint16, "w+", 0, size)
+        i = 0
+        for x in tokens:
+            n = len(x)
+            mmap[i : i + n] = x
+            i += n
+        mmap.flush()
