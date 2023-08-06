0b11a87da02618a4dcacda6772903dcf24f72fdd
Thien Tran <gau.nernst@yahoo.com.sg>
2023-08-06 13:04:42 +0800

    update tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("action")
     parser.add_argument("--input", required=True)
-    parser.add_argument("--save_path")
+    parser.add_argument("--save_name")
     parser.add_argument("--model_name", required=True)
     parser.add_argument("--model_type", default="unigram")
     parser.add_argument("--vocab_size", type=int, default=8000)
@@ -30,18 +30,20 @@ if __name__ == "__main__":
         )
 
     elif args.action == "tokenize":
-        assert args.save_path is not None
+        assert args.save_name is not None
         tokenizer = spm.SentencePieceProcessor(f"{args.model_name}.model")
         vocab_size = tokenizer.vocab_size()
-        assert vocab_size < 2**16
+        assert vocab_size < 2**15  # int16
 
         stories = open(args.input).read().split("<|endoftext|>")
-        tokens = tokenizer.Encode(stories, add_bos=True)
-        size = sum(len(x) for x in tokens)
-        mmap = np.memmap(args.save_path, np.uint16, "w+", 0, size)
+        stories = [x for x in stories if len(x) >= 10]  # remove very short stories
+        tokens_list = tokenizer.Encode(stories, add_bos=True)
+
+        size = sum(len(x) for x in tokens_list)
+        data = np.memmap(f"{args.save_name}.bin", np.int16, "w+", 0, size)
         i = 0
-        for x in tokens:
-            n = len(x)
-            mmap[i : i + n] = x
-            i += n
-        mmap.flush()
+
+        for tokens in tokens_list:
+            data[i : i + len(tokens)] = tokens
+            i += len(tokens)
+        data.flush()
